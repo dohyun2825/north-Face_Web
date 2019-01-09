4338065a94427ad22f7d27da20e6464b2bf0ebd5
dohyun2825 <31638705+dohyun2825@users.noreply.github.com>
2019-01-09 14:28:15 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.logo a img').click(
+    function(){
+        location.href="index.html";
+    });
+    
+    $('.siteMap').click(
+    function(){
+        location.href="sub/sitemap.html";
+    });
+    
+    $('.gnb li:nth(0)').click(
+    function(){
+        location.href="sub/info.html";
+    });
+    
+    $('.gnb li:nth(1)').click(
+    function(){
+        location.href="sub/products.html";
+    });
+    
+    $('.gnb li:nth(2)').click(
+    function(){
+        location.href="sub/community.html";
+    });
+    
+    $('.gnb li:nth(3)').click(
+    function(){
+        location.href="sub/products.html";
+    });
+    
+    $('.gnb li:nth(4)').click(
+    function(){
+        location.href="sub/products.html";
+    });
+    
+    $('.gnb li:nth(5)').click(
+    function(){
+        location.href="sub/help.html";
+    });
+});
---
+++ js/sub.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    
+    $('.logo a img').click(
+    function(){
+        location.href="../index.html";
+    });
+    
+    $('.siteMap').click(
+    function(){
+        location.href="sitemap.html";
+    });
+    
+    $('.gnb li:nth(0)').click(
+    function(){
+        location.href="info.html";
+    });
+    
+    $('.gnb li:nth(1)').click(
+    function(){
+        location.href="products.html";
+    });
+    
+    $('.gnb li:nth(2)').click(
+    function(){
+        location.href="community.html";
+    });
+    
+    $('.gnb li:nth(3)').click(
+    function(){
+        location.href="products.html";
+    });
+    
+    $('.gnb li:nth(4)').click(
+    function(){
+        location.href="products.html";
+    });
+    
+    $('.gnb li:nth(5)').click(
+    function(){
+        location.href="help.html";
+    });
+    
+});
